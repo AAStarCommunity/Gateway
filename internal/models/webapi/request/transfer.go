97438a587f8d9038add09cdff8bce0cd2fc7d2ce
Jhfnetboy(Survivor) <jhfnetboy@gmail.com>
2023-11-18 21:19:44 +0300

    add models functions and response, go home!

---
+++ internal/models/webapi/request/transfer.go
@@ -0,0 +1,6 @@
+package request
+
+type Transfer struct {
+	Receiver string // 接收人ID
+	Value    string // 转账金额
+}
---
+++ internal/models/webapi/response/model.go
@@ -0,0 +1,65 @@
+package response
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func GetResponse() *Response {
+	return &Response{
+		httpCode: http.StatusOK,
+		Result: &Result{
+			Code:    0,
+			Message: "",
+			Data:    nil,
+			Cost:    "",
+		},
+	}
+}
+func BadRequest(ctx *gin.Context, data ...any) {
+	GetResponse().withDataAndHttpCode(http.StatusBadRequest, ctx, data)
+}
+
+// Success 业务成功响应
+func Success(ctx *gin.Context, data ...any) {
+	if data != nil {
+		GetResponse().WithDataSuccess(ctx, data[0])
+		return
+	}
+	GetResponse().Success(ctx)
+}
+
+// Fail 业务失败响应
+func Fail(ctx *gin.Context, code int, message *string, data ...any) {
+	var msg string
+	if message == nil {
+		msg = ""
+	} else {
+		msg = *message
+	}
+	if data != nil {
+		GetResponse().WithData(data[0]).FailCode(ctx, code, msg)
+		return
+	}
+	GetResponse().FailCode(ctx, code, msg)
+}
+
+type Result struct {
+	Code    int         `json:"code"`
+	Message string      `json:"message"`
+	Data    interface{} `json:"data"`
+	Cost    string      `json:"cost"`
+}
+
+type Response struct {
+	httpCode int
+	Result   *Result
+}
+
+// Fail 错误返回
+func (r *Response) Fail(ctx *gin.Context) *Response {
+	r.SetCode(http.StatusInternalServerError)
+	r.json(ctx)
+	return r
+}
